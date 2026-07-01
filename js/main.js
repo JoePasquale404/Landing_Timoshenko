ad5be5391419b77c4a14f40ed78e95b24d3c8dc4
JoePasquale404 <aleksandrsemenov5256@gmail.com>
2026-07-01 15:43:38 +0700

    сделан адаптив для секции постеры

--- js/main.js
+++ js/main.js
@@ -45,11 +45,16 @@
 })();
 
 new Swiper('.posters__slider', {
-    slidesPerView: 4,
+    slidesPerView: 2,
     spaceBetween: 20,
     navigation: {
         nextEl: '.posters__next',
         prevEl: '.posters__prev',
+    },
+    breakpoints: {
+        700: {
+            slidesPerView: 4,
+        },
     },
 });
 
